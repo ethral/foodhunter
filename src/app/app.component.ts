65512dd774075d8497bba5695d3e69969df6d49e
Abhinav Sudam <abhinavsudam1@gmail.com>
2018-12-11 18:32:10 -0500

    Firebase addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'foodhunter';
+
+  ngOnInit(){
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyBpXl6stJ9gxU5PZFXPVOw_owheclajFbc",
+      authDomain: "foodhunter-db.firebaseapp.com"
+    });
+
+  }
 }
