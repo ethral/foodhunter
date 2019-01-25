2747e8245d2f1ec38f29627ca38731b4d75b63a8
edodso2 <2.3turbofox@gmail.com>
2019-01-24 19:20:08 -0500

    remove unused imports and fix tslint errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
 import { FoodmapComponent } from './foodmap/foodmap.component';
-import { FoodmapEditComponent } from './foodmap/foodmap-edit/foodmap-edit.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HomeComponent } from './home/home.component';
 
-
-
-
-
-
-
 const appRoutes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  {path : 'foodmaps', component: FoodmapComponent},
-
-  {path : 'signup', component: SignupComponent},
-  {path : 'signin', component: SigninComponent}
-  
+  { path: 'foodmaps', component: FoodmapComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: SigninComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule { }
