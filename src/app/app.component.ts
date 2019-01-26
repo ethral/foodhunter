2710a3522db8d4af9884dcfe672884a734b3a717
edodso2 <2.3turbofox@gmail.com>
2019-01-25 22:20:38 -0500

    remove api keys and add config file to gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
 
+import { config } from 'src/firebase.config';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,9 +14,6 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     // firebase SDK getting initialized during the start up of the app
     // auth functionality provided out of the box by firebase
-    firebase.initializeApp({
-      apiKey: 'AIzaSyBpXl6stJ9gxU5PZFXPVOw_owheclajFbc',
-      authDomain: 'foodhunter-db.firebaseapp.com'
-    });
+    firebase.initializeApp(config);
   }
 }
