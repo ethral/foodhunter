ca6f291eb7922ec8d5a998c6780c740009cda78a
edodso2 <2.3turbofox@gmail.com>
2019-01-25 22:01:33 -0500

    test poor formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent implements OnInit {
   title = 'foodhunter';
 
   ngOnInit() {
-    //firebase SDK getting initialized during the start up of the app
+    // firebase SDK getting initialized during the start up of the app
     // auth functionality provided out of the box by firebase
     firebase.initializeApp({
       apiKey: 'AIzaSyBpXl6stJ9gxU5PZFXPVOw_owheclajFbc',
